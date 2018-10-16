deda982ab41c588cdbf7d375aff3b908638e2bba
Juanmi Alberola <jmalberola@florida-uni.es>
2018-10-16 13:02:21 +0200

    Ejemplos de clase

--- funciones/js/codigo.js
+++ funciones/js/codigo.js
@@ -7,3 +7,10 @@ function notaFinal(nota1,nota2,nota3){
 var calificacion=0;
 calificacion=notaFinal(8,9,10);
 console.log(calificacion);
+
+//Usando la funcion the revenge
+var n1=Number(prompt("Dame nota 1"));
+var n2=Number(prompt("Dame nota 2"));
+var n3=Number(prompt("Dame nota 3"));
+calificacion=notaFinal(n1,n2,n3);
+console.log(calificacion);
