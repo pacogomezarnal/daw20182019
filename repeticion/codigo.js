b8f834f8f04b747eb93fde884492b0d46b8ae398
Juanmi Alberola <jmalberola@florida-uni.es>
2018-10-16 11:58:37 +0200

    Ejemplos de clase

---
+++ repeticion/codigo.js
@@ -0,0 +1,12 @@
+var numerosPares=0;
+var contador=0;
+while(contador<10){
+  numerosPares=numerosPares+2;
+  console.log(numerosPares);
+  contador=contador+1;
+}
+var numerosPares=0;
+for (var contador = 0; contador<10; contador++) {
+  numerosPares=numerosPares+2;
+  console.log(numerosPares);
+}
