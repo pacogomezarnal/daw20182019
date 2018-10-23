b8a1d064ad2df4d0ca5fd4d3c64d1c3aa66cb4e6
Juanmi Alberola <jmalberola@florida-uni.es>
2018-10-23 11:56:59 +0200

    Arrays inicio

---
+++ arrays/js/codigo.js
@@ -0,0 +1,10 @@
+//DEfinicion con arrays
+var pseudorestaurantes=["mcdonalds","burguerking","tgb"];
+var pseudesayunos=Array("colacao","nesquik","cacaoHacendado");
+var edadAlumnos=[18,17,38,10];
+//DEfinicion sin arrays
+var pseudorestaurantes1="mcdonalds";
+var pseudorestaurantes2="burguerking";
+var pseudorestaurantes3="tgb";
+//Muestro por pantalla
+console.log(edadAlumnos);
