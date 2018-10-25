dda310e8a1bfee3dc3dd7baa52cdfdc7292aec57
Juanmi Alberola <jmalberola@florida-uni.es>
2018-10-25 11:54:33 +0200

    Ejemplos sobre funciones

---
+++ ejemplosFunciones/js/codigo.js
@@ -0,0 +1,11 @@
+//PRIMER EJEMPLO
+function suma(a,b){
+  let total=a+b;
+  return total;
+}
+
+//USANDO LAS FUNCIONES
+var num1=Number(prompt("Dame el primer numero"));
+var num2=Number(prompt("Dame el primer numero"));
+console.log(suma(num1,num2));
+console.log(suma(3,-4));
